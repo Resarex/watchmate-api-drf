8013107dc57000caec2d9a83a55999ce64bf012a
sidhant yadav <sidhant2yadav@gmail.com>
2025-10-20 19:12:44 +0530

    Fix build.sh requirements.txt path for Render

--- watchmate/build.sh
+++ watchmate/build.sh
@@ -2,8 +2,10 @@
 # exit on error
 set -o errexit
 
-pip install -r ../requirements.txt
+pip install -r requirements.txt
+
+cd watchmate
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py create_superuser
+python manage.py create_superuser 
